0e61bcd184cb98ca6bfd56c165cc80edeb68cc60
mehedifiz <mehedi.shanto.contract@gmail.com>
2025-01-03 18:31:25 +0600

    api for delete blogs

--- index.js
+++ index.js
@@ -158,8 +158,14 @@ async function run() {
       res.send(result);
     });
 
+    //api for delete a blog
 
-
+    app.delete("/delete-blog/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await blogsCollection.deleteOne(query);
+      res.send(result);
+    });
 
     // applied related api.
 
